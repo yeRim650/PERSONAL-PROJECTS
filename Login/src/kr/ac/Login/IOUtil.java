cbfcc405b519a02e33224d3ce5c4d1c17443fce2
yerim <forger980@gmail.com>
2022-06-09 20:37:47 +0900

    _

---
+++ Login/src/kr/ac/Login/IOUtil.java
@@ -0,0 +1,13 @@
+package kr.ac.Login;
+
+import java.io.Closeable;
+
+public class IOUtil {
+	public static void closeAll(Closeable...c){
+		for(Closeable temp : c){
+			try{
+				temp.close();
+			}catch(Exception e){}
+		}
+	}
+}
